e55f1a8acb82e17bd4884e16065adddf05c42421
GoncaloRego <goncalo-fcp@hotmail.com>
2016-11-22 09:48:30 +0000

    ...

---
+++ IP3D-TPF/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP3D_TPF
+{
+    public class Bullet
+    {
+        BasicEffect effect;
+        public Bullet(GraphicsDevice device)
+        {
+            effect = new BasicEffect(device);
+        }
+    }
+}
